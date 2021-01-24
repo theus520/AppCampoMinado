69f96f1454eeef93e6e48f512b5d64dde8005869
theus520 <matheusrabelo520@gmail.com>
2021-01-24 09:41:22 -0300

    inserindo a flag no campo minado

--- App.js
+++ App.js
@@ -14,14 +14,21 @@ export default class App extends Component {
     </Text>
 
   <Field />
-
-      </View>
+  <Field opened />
+  <Field opened nearMines={1} />
+  <Field opened nearMines={2} />
+  <Field opened nearMines={3} />
+  <Field opened nearMines={6} />
+  <Field mined /> 
+  <Field mined opened />
+  <Field mined opened exploded />
+  <Field flagged />
+  <Field flagged opened />
+      </View> 
     );
   }
 }
  
-
-
 const styles = StyleSheet.create({
   container: {
    flex: 1,
--- src/components/Field.js
+++ src/components/Field.js
@@ -1,21 +1,44 @@
 import React from 'react'
 import { View, StyleSheet,Text} from 'react-native'
 import params from '../params'
+import Mine from './Mine'
+import Flag from './Flag'
 
 
 export default props => {
 
+    const {mined, opened, nearMines,exploded,flagged} = props
+
     const styleField = [styles.field]
-    // outros estilos aqui ! 
-    if(styleField.length === 1 ) styleField.push(styles.regular)
-   
+
+    if( opened) styleField.push(styles.opened)
+     if(exploded) styleField.push(styles.exploded)
+     if(flagged) styleField.push(styles.flagged)
+
+    if(!opened && ! exploded ) styleField.push(styles.regular)
+   let color = null
+
+   if(nearMines > 0){
+       if(nearMines == 1) color = '#2A28D7'
+       if(nearMines == 2) color = '#2B520F'
+       if(nearMines > 2  && nearMines < 6 ) color = '#2F906A'
+       if (nearMines => 6) color = '#F221A9'
+    }
+
+
     return (
         <View style={styleField}>
+        {!mined && opened && nearMines > 0 ? 
+        <Text style={[styles.label, {color: color}]}>
+        {nearMines} </Text> : false}
 
-
+        {mined && opened ? <Mine /> : false}
+     
+        {flagged && !opened ? <Flag /> : false}
+        
         </View>
     )
-
+         
 }
 
 const styles = StyleSheet.create({
@@ -30,6 +53,21 @@ regular: {
     borderTopColor: '#CCC',
     borderRightColor: '#333',
     borderBottomColor: '#333',
+},
+opened: {
+    backgroundColor: '#999',
+    borderColor: '#777',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+label:{
+    fontWeight: 'bold',
+    fontSize: params.fontSize,
+
+},
+exploded: {
+     backgroundColor: 'red',
+     borderColor: 'red',
 }
 })
 
